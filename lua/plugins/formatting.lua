abc46324f86d7ab6cc53b736f49bc9f3b539d414
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2024-10-03 17:06:34 -0300

    first commit

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,34 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				rust = { "rustfmt" },
+			},
+			format_after_save = {
+				lsp_fallback = true,
+				async = true,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = true,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
